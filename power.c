4a98731c46cc6027f57173a95d9b21949367d45b
Arnab Ghosh <43007068+ArnabG99@users.noreply.github.com>
2019-04-23 10:39:44 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,21 @@
+#include<stdio.h> //Q12
+int myPower(int a,int b);
+int main()
+{
+    int a,b,value;
+    printf("enter the value of a and b: \n");
+    scanf("%d%d",&a,&b);
+    value=myPower(a,b);
+    printf("the value of %d raised to %d is %d",a,b,value);
+    return 0;
+}
+int myPower(int a,int b)
+{
+    int i,value;
+    value=a;
+    for(i=1;i<b;i++)
+    {
+    value=value*a;
+    }
+    return value;
+}
